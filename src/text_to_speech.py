e2ed2007ce1636e57955fb1d22ab2e632bf0bd66
Tejas Jaiswal <114175862+tejasjaiswal07@users.noreply.github.com>
2024-09-10 14:36:59 +0530

    Update text_to_speech.py

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,10 +1,8 @@
-# text_to_speech.py
+
 from gtts import gTTS
 import os
 
 def text_to_speech(text):
     tts = gTTS(text=text, lang='en')
     tts.save("response.mp3")
-    os.system("start response.mp3")  # On Windows
-    # os.system("afplay response.mp3")  # On macOS
-    # os.system("mpg321 response.mp3")  # On Linux
+    os.system("start response.mp3") 
